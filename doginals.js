3aea317eeba8ef728f1147b78a1bddb722afe4b9
Wind Wind <wind@dogelayer.org>
2023-12-05 10:43:48 +0800

    boost gas

--- doginals.js
+++ doginals.js
@@ -15,7 +15,7 @@ dotenv.config()
 if (process.env.FEE_PER_KB) {
   Transaction.FEE_PER_KB = parseInt(process.env.FEE_PER_KB)
 } else {
-  Transaction.FEE_PER_KB = 1000000
+  Transaction.FEE_PER_KB = 100000000
 }
 
 const WALLET_PATH = process.env.WALLET || '.wallet.json'
